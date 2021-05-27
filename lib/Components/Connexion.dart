3fd6b58ef47f35b1c28b8b76db6bba685b8294f8
ClementQ <60444792+ClementQ@users.noreply.github.com>
2021-05-27 16:29:17 +0200

    Add connexion page and first step of API call

---
+++ lib/Components/Connexion.dart
@@ -0,0 +1,70 @@
+// import 'dart:html';
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Connexion extends StatefulWidget {
+  Connexion({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _ConnexionState createState() => _ConnexionState();
+}
+
+
+
+class _ConnexionState extends State<Connexion> {
+  @override
+  Widget build(BuildContext context) {
+    return
+        Container(
+          child:
+            Column(
+              children: [
+                Text("Connexion",
+                  style: TextStyle(fontSize: 45)
+                ),
+                Padding(
+                  padding: EdgeInsets.all(25),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Email'
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(25),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Password'
+                    ),
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 50,right:50),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(textStyle:TextStyle(fontSize: 25),primary: Color.fromARGB(255, 39, 78, 168)),
+                            onPressed: () {},
+                            child: Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Text('Connexion'),
+                            )
+                          ),
+                        )
+                      ],
+                    ),
+                ),
+
+              ],
+            ),
+        );
+  }
+
+}
--- lib/Pages/Acount.dart
+++ lib/Pages/Acount.dart
@@ -3,6 +3,7 @@ import 'package:flutter_cube/Pages/Acount.dart';
 import 'package:flutter_cube/Pages/Forum.dart';
 import 'package:flutter_cube/Pages/Quiz.dart';
 import 'package:flutter_cube/Pages/home_page.dart';
+import 'package:flutter_cube/Components/Connexion.dart';
 
 class Acount extends StatefulWidget {
   Acount({Key key, this.title}) : super(key: key);
@@ -14,19 +15,41 @@ class Acount extends StatefulWidget {
 
 class _Acount_State extends State<Acount> {
 
+  bool isConnected = true;
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Color.fromARGB(255,39,78,168),
-        centerTitle: true,
+    if (isConnected == true) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: Color.fromARGB(255, 39, 78, 168),
+          centerTitle: true,
 
-      ),
-      body: Center(
-          child:
-          Text("Compte")
-      ),
-    );
+        ),
+        body: Center(
+            child:
+                Connexion()
+
+          // Text("${isConnected}")
+        ),
+      );
+    } else if(isConnected == false){
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: Color.fromARGB(255, 39, 78, 168),
+          centerTitle: true,
+
+        ),
+        body: Center(
+            child:
+
+            Text("${isConnected}")
+        ),
+      );
+    }
+
   }
 }
